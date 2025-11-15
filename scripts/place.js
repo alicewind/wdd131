08f72b3a6a943c3f6e3a0a10f2d8a63aa2623e4c
alicewind <awind@byupathway.edu>
2025-11-15 14:31:10 -0700

    full code

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,21 @@
+const temp = document.querySelector(".temp-value");
+const wind = document.querySelector(".wind-value");
 
+function calculateWindChill(temp, wind) {
+    const windChillFactor = 13.12 + (0.6215 * temp) - (11.37 * wind ** .16) + (.3965 * temp * wind ** .16);
+    return windChillFactor;
+}
 
+const temperature = parseFloat(temp.textContent);
+const windSpeed = parseFloat(wind.textContent);
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    const windChillElement = document.querySelector(".wind-chill-value");
+    const windChill = calculateWindChill(temperature, windSpeed);
+    windChillElement.textContent = windChill.toFixed(1);
+} else {
+    document.querySelector(".wind-chill-value").textContent = "N/A";
+}
 
 const year = document.querySelector("#currentyear");
 const today = new Date();
