57fec0d4d97eb0df145d14297e2d8cc270615d6e
alicewind <awind@byupathway.edu>
2025-12-04 19:50:53 -0700

    about us page

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -12,3 +12,57 @@ window.addEventListener('resize', () => {
         hambutton.classList.remove('show');
     }
 });
+
+const lessons = [
+    {
+        id: "Session 1",
+        dates: "April 6-15",
+        level: "Beginner"
+    },
+    {
+        id: "Session 2",
+        dates: "April 20-29",
+        level: "Beginner"
+    },
+    {
+        id: "Session 3",
+        dates: "May 4-13",
+        level: "Beginner"
+    },
+    {
+        id: "Session 4",
+        dates: "May 18-27",
+        level: "Advanced"
+    },
+    {
+        id: "Session 5",
+        dates: "June 1-10",
+        level: "Beginner"
+    },
+    {
+        id: "Session 6",
+        dates: "June 15-24",
+        level: "Beginner"
+    },
+    {
+        id: "Session 7",
+        dates: "July 6-15",
+        level: "Beginner"
+    },
+    {
+        id: "Session 8",
+        dates: "July 20-29",
+        level: "Advanced"
+    }
+];
+
+const lessonsSelect = document.querySelector("#lessonSession");
+
+if (lessonsSelect) {
+    lessons.forEach(lesson => {
+        const option = document.createElement("option");
+        option.textContent = `${lesson.id} - ${lesson.dates} (${lesson.level})`;
+        lessonsSelect.appendChild(option);
+    })
+}
+
